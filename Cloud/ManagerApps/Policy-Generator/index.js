80d61fde8521dd182c256f96f5e6fafda9e379cf
thomas.man-gang.cheung@schibsted.com <thomas.man-gang.cheung@schibsted.com>
2022-01-02 17:32:37 +0100

    Formatting, small fixes

--- Cloud/ManagerApps/Policy-Generator/index.js
+++ Cloud/ManagerApps/Policy-Generator/index.js
@@ -1,112 +1,99 @@
-const { Kafka } = require('kafkajs')
-const MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
+const { Kafka } = require("kafkajs");
+const MongoClient = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectID;
 
+const { render_tenant_policy } = require("./policy_pug.js");
 
-const { render_tenant_policy } = require('./policy_pug.js')
+const BOOTSTRAP_KAFKA_HOST =
+    process.env.BOOTSTRAP_KAFKA_HOST || "192.168.1.156:9092";
+const MONGO_URL =
+    process.env.MONGO_URL || "mongodb://root:password123@192.168.1.156";
+const MONGO_DBNAME = process.env.MONGO_DBNAME || "datahub";
 
-
-const BOOTSTRAP_KAFKA_HOST = process.env.BOOTSTRAP_KAFKA_HOST || "192.168.1.156:9092"
-const MONGO_URL = process.env.MONGO_URL || "mongodb://root:password123@192.168.1.156"
-const MONGO_DBNAME = process.env.MONGO_DBNAME || "datahub"
-
-const POLICY_TOPIC = ".tenant_policy"
+const POLICY_TOPIC = ".tenant_policy";
 
 const kafka = new Kafka({
-	clientId: 'policy-generator',
-	brokers: [BOOTSTRAP_KAFKA_HOST]
-})
-
-
-
-
-const producer = kafka.producer()
-producer.connect()
-const admin = kafka.admin()
-admin.connect()
-
-async function generate_policy(tenant_id, policies) {
-
-
-
-
-	console.log(`Updating policy for tenant ${tenant_id}`)
-
-	const tenant_policy = {
-		policies: render_tenant_policy(policies),
-		tenant_id: tenant_id
-	}
-
-
-	let ts = Date.now();
-	console.log(`[${tenant_id}], Policy Generate TS: ${ts}`);
-
-	producer.send({
-		topic: POLICY_TOPIC,
-		waitForLeaders: true,
-		messages: [
-			{
-				value: JSON.stringify(tenant_policy)
-			},
-		],
-	})
-}
-
-
-
-
-
-MongoClient.connect(MONGO_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-}, function (err, db) {
-	if (err) throw err;
-	var dbo = db.db(MONGO_DBNAME);
-
-
-
-	//Watch for changes
-	changeStream = dbo.collection("policies").watch();
-
-	changeStream.on("change", function (change) {
-		documentKey = change['documentKey']['_id'];
-
-
-		dbo.collection("policies").findOne({ "_id": new ObjectId(documentKey) }, function (err, doc) {
-			if (doc != undefined) {
-
-				const ts = Date.now();
-				console.log(`[${doc['tenant_name']}], Policy Change TS: ${ts}`);
-
-				tenant_id = doc['tenant_name']
-				policies = doc['policies']
-				generate_policy(tenant_id, policies)
-			}
-		});
-	});
-
-
-
-	//Regenerate the policy at once
-	dbo.collection("policies").find({}).toArray(function (err, result) {
-
-
-
-		if (err) throw err;
-
-		result.forEach(doc => {
-			const ts = Date.now();
-			console.log(`[${doc['tenant_name']}], Policy Generate TS: ${ts}`);
-
-
-			tenant_id = doc['tenant_name']
-			policies = doc['policies']
-
-			generate_policy(tenant_id, policies)
-
-		});
-
-	});
-
+    clientId: "policy-generator",
+    brokers: [BOOTSTRAP_KAFKA_HOST],
 });
 
+const producer = kafka.producer();
+producer.connect();
+const admin = kafka.admin();
+admin.connect();
+
+async function generate_policy(tenant_id, policies) {
+    console.log(`Updating policy for tenant ${tenant_id}`);
+
+    const tenant_policy = {
+        policies: render_tenant_policy(policies),
+        tenant_id: tenant_id,
+    };
+
+    let ts = Date.now();
+    console.log(`[${tenant_id}], Policy Generate TS: ${ts}`);
+
+    producer.send({
+        topic: POLICY_TOPIC,
+        waitForLeaders: true,
+        messages: [
+            {
+                value: JSON.stringify(tenant_policy),
+            },
+        ],
+    });
+}
+
+MongoClient.connect(
+    MONGO_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    function (err, db) {
+        if (err) throw err;
+        var dbo = db.db(MONGO_DBNAME);
+
+        //Watch for changes
+        changeStream = dbo.collection("policies").watch();
+
+        changeStream.on("change", function (change) {
+            documentKey = change["documentKey"]["_id"];
+
+            dbo.collection("policies").findOne(
+                { _id: new ObjectId(documentKey) },
+                function (err, doc) {
+                    if (doc != undefined) {
+                        const ts = Date.now();
+                        console.log(
+                            `[${doc["tenant_name"]}], Policy Change TS: ${ts}`
+                        );
+
+                        tenant_id = doc["tenant_name"];
+                        policies = doc["policies"];
+                        generate_policy(tenant_id, policies);
+                    }
+                }
+            );
+        });
+
+        //Regenerate the policy at once
+        dbo.collection("policies")
+            .find({})
+            .toArray(function (err, result) {
+                if (err) throw err;
+
+                result.forEach((doc) => {
+                    const ts = Date.now();
+                    console.log(
+                        `[${doc["tenant_name"]}], Policy Generate TS: ${ts}`
+                    );
+
+                    tenant_id = doc["tenant_name"];
+                    policies = doc["policies"];
+
+                    generate_policy(tenant_id, policies);
+                });
+            });
+    }
+);
--- Tenants/Tenant-Web/src/PeopleCountApp.js
+++ Tenants/Tenant-Web/src/PeopleCountApp.js
@@ -1,72 +1,57 @@
-
-import './App.css';
-import mqtt from 'mqtt';
-
-
+import "./App.css";
+import mqtt from "mqtt";
 import DataFrame from "dataframe-js";
-
-import React from 'react';
+import React from "react";
 
 class PeopleCountApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { people_count: 0 };
 
-  constructor(props) {
-    super(props);
-    this.state = { people_count: 0 };
+        this.df = new DataFrame([], ["Timestamp", "data"]);
+    }
 
-    this.df = new DataFrame([], ["Timestamp", "data"])
+    componentDidMount() {
+        const client = mqtt.connect("ws://192.168.1.156:8888", {
+            username: this.props.username,
+            password: this.props.password,
+        });
+        client.on("connect", function () {
+            console.log("Connected");
+            client.subscribe(
+                "/smartcity/camera/people_count/usa/ohio/store_x/city_ai",
+                function (err) {}
+            );
+        });
 
+        client.on("message", (topic, payload, packet) => {
+            // console.log(payload.toString());
+            this.df = this.df.push([Date.now(), payload.toString()]);
+            this.df.show();
+            const last_5mins = Date.now() - 5 * 60 * 1000;
 
-  }
+            this.setState({
+                people_count: payload.toString(),
+                max_5mins: this.df
+                    .filter((row) => row.get("Timestamp") > last_5mins)
+                    .stat.max("data"),
+            });
+        });
+    }
 
-
-  componentDidMount() {
-
-    const client = mqtt.connect('ws://192.168.1.156:8888', { username: this.props.username, password: this.props.password });
-
-    client.on('connect', function () {
-      console.log("Connected")
-
-      client.subscribe('/smartcity/camera/people_count/usa/ohio/store_x/city_ai', function (err) {
-
-      })
-    });
-
-
-    client.on('message', (topic, payload, packet) => {
-
-      // console.log(payload.toString());
-
-      this.df = this.df.push([Date.now(), payload.toString()])
-
-      this.df.show();
-
-      const last_5mins = Date.now() - 5 * 60 * 1000
-
-
-
-      this.setState({
-        people_count: payload.toString(),
-        max_5mins: this.df.filter(row => row.get('Timestamp') > last_5mins).stat.max("data")
-      });
-
-    });
-
-
-
-  }
-  render() {
-
-    return (
-
-
-      <span>
-        People Count: {this.state.people_count || 0}  (Max last 5 minutes: <span className="text-danger"> {this.state.max_5mins || 0} </span>)
-      </span>
-
-
-    );
-  }
+    render() {
+        return (
+            <span>
+                People Count: {this.state.people_count || 0} (Max last 5
+                minutes:{" "}
+                <span className="text-danger">
+                    {" "}
+                    {this.state.max_5mins || 0}{" "}
+                </span>
+                )
+            </span>
+        );
+    }
 }
-
 
 export default PeopleCountApp;
