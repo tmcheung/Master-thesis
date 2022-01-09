8a65d4f9e9952854a6e71dc5c2bcd6b053ba4e4c
thomas.man-gang.cheung@schibsted.com <thomas.man-gang.cheung@schibsted.com>
2022-01-09 09:21:21 +0700

    formatting and expose OPA for debugging

--- Cloud/ManagerApps/Policy-Generator/copy_tenants.js
+++ Cloud/ManagerApps/Policy-Generator/copy_tenants.js
@@ -1,47 +1,41 @@
-const MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
+const MongoClient = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectID;
 
-const MONGO_URL = process.env.MONGO_URL || "mongodb://root:root@192.168.1.156"
-const MONGO_DBNAME = process.env.MONGO_DBNAME || "datahub"
+const MONGO_URL = process.env.MONGO_URL || "mongodb://root:root@192.168.1.156";
+const MONGO_DBNAME = process.env.MONGO_DBNAME || "datahub";
 
+MongoClient.connect(
+    MONGO_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    function (err, db) {
+        if (err) throw err;
+        var dbo = db.db(MONGO_DBNAME);
 
+        dbo.collection("policies").findOne(
+            { tenant_name: "tenant-3" },
+            function (err, result) {
+                if (err) throw err;
 
-MongoClient.connect(MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, function (err, db) {
-  if (err) throw err;
-  var dbo = db.db(MONGO_DBNAME);
+                const tenant_idx = Math.floor(+new Date());
 
+                delete result["_id"];
 
+                console.log(result);
 
+                result["tenant_name"] = `tenant-${tenant_idx}`;
 
-  dbo.collection("policies").findOne({ tenant_name: "tenant-3" }, function (err, result) {
-    if (err) throw err;
-
-    const tenant_idx = Math.floor(+new Date())
-
-    delete result['_id']
-
-    console.log(result)
-
-    result['tenant_name'] = `tenant-${tenant_idx}`
-
-
-    dbo.collection("policies").insertOne(result, function (err, res) {
-      if (err) throw err;
-      console.log("1 document inserted");
-      db.close();
-
-    });
-
-
-
-
-
-
-
-
-  });
-
-});
+                dbo.collection("policies").insertOne(
+                    result,
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log("1 document inserted");
+                        db.close();
+                    }
+                );
+            }
+        );
+    }
+);
